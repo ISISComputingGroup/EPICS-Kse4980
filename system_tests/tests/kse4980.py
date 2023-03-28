df0e87c202b30815f1696bccb59aceb292c8eb6d
Jack Harper <jack.harper@stfc.ac.uk>
2023-03-28 15:58:37 +0100

    add sigs for tests

--- system_tests/tests/kse4980.py
+++ system_tests/tests/kse4980.py
@@ -30,8 +30,27 @@ class Kse4980Tests(unittest.TestCase):
         self._lewis, self._ioc = get_running_lewis_and_ioc("Kse4980", DEVICE_PREFIX)
         self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX, default_wait_time=0.0)
 
-    def test_that_fails(self):
-        self.fail("You haven't implemented any tests!")
+    def test_GIVEN_setting_a_current_WHEN_in_voltage_mode_THEN_signal_type_switches(self):
+        self.ca.set_pv_value("SIGNALTYPE:SP", "CURR")
+        self.ca.assert_that_pv_is("SIGNALTYPE", "CURR")
+    
+    def test_GIVEN_invalid_function_WHEN_setting_function_THEN_function_is_not_set_and_error_id_populated(self):
+        pass
+
+    def test_GIVEN_reset_all_button_pressed_WHEN_non_standard_settings_applied_THEN_defaults_are_restored(self):
+        pass
+
+    def test_GIVEN_setting_impedance_range_THEN_imprange_is_updated(self):
+        pass
+
+    def test_GIVEN_averaging_factor_set_THEN_averaging_factor_applied_on_device(self):
+        pass
+
+    def test_GIVEN_measurement_time_changed_THEN_averaging_factor_and_measurement_time_both_updated(self):
+        pass
+
+    def test_GIVEN_both_readings_update_on_device_WHEN_polling_THEN_reading_pvs_updated(self):
+        pass
 
 '''
 tests
